768a5ad00909a9ce2edec2e0a5567a6b3c159df4
Fire Game <kudzejcookc@gmail.com>
2026-06-02 20:48:59 +0200

    changeing coal to flamable grain

--- public/js/classes/grains.js
+++ public/js/classes/grains.js
@@ -202,10 +202,10 @@ class FlamableGrain extends Grain {
 	}
 }
 
-class Coal extends Grain{
+class Coal extends FlamableGrain {
 	chanceToDie = 0.05;
 	constructor (gravity = 0, name = "Coal", chanceToDie = 0.1, flammability = 80){
-		super(gravity, 0, 6, name);
+		super(gravity, 0, 6, name, 0);
 		this.chanceToDie = chanceToDie;
 		this.flammability = flammability;
 
