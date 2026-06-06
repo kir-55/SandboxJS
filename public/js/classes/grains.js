0a3ad1a614bf4d08fd8ea2b83779ebeaa1c99654
Fire Game <kudzejcookc@gmail.com>
2026-06-06 15:10:50 +0200

    makeing trees spawn more often

--- public/js/classes/grains.js
+++ public/js/classes/grains.js
@@ -1281,7 +1281,7 @@ class Dirt extends LiquidAffectable {
 }
 
 class WetDirt extends LiquidAffectable {
-	constructor(wetGrain = null, dryGrain = null, chanceToGrowGrass = 0.0001, chanceToGrowTree = 0.00001, name = "Wet Dirt") {
+	constructor(wetGrain = null, dryGrain = null, chanceToGrowGrass = 0.0001, chanceToGrowTree = 0.00003, name = "Wet Dirt") {
 		super(0, 0, 3, wetGrain, dryGrain, 0, 0, false, true, 0.01, name);
 		this.chanceToGrowGrass = chanceToGrowGrass;
 		this.chanceToGrowTree = chanceToGrowTree;
